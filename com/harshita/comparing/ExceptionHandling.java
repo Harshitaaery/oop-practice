418d0265ac5cc2696100e8da8913d2b73c187ed9
Harshitaaery <aeryharshita61@gmail.com>
2025-09-15 01:45:01 +0530

    done

--- com/harshita/comparing/ExceptionHandling.java
+++ com/harshita/comparing/ExceptionHandling.java
@@ -9,10 +9,22 @@ class Main{
         int a = 5;
         int b = 0;
         try{
-           int c = a/b;
-
-        } catch (Exception e){
-            System.out.println("that's not possible girl");
+           divide(a,b);
+        } catch (ArithmeticException e){ //stricter, less general ones first
+            System.out.println(e.getMessage());
+        } catch (Exception e){ //generalexception later because it'll catch all exception so it wouldn't make sense to put it first
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("it's okay you'll learn");
         }
+
+    }
+
+    static int divide(int a, int b) throws ArithmeticException{
+        if(b == 0){
+            throw new ArithmeticException( "please don't do this uwu");
+        }
+
+        return a/b;
     }
 }
